bb703e5cbb7911706dd3c880ad3bfdfa523e2a81
Digitronn <felixjchirinos@outlook.com>
2022-07-05 00:11:15 +0200

    folder names corrected

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,32 +1,37 @@
-import express from "express";
-import {createUser } from "../user-controller/signupController.js"
-import {loginUser } from "../user-controller/signinController.js"
-import {postHistory, getHistory, deleteHistory,updateHistory, loggedIn } from "../user-controller/historyController.js"
-import auth from "../middle/auth.js";
-import { body } from "express-validator";
+import express from "express"
+import { createUser } from "../controllers/signupController.js"
+import { loginUser } from "../controllers/signinController.js"
+import {
+  postHistory,
+  getHistory,
+  deleteHistory,
+  updateHistory,
+  loggedIn,
+} from "../controllers/historyController.js"
+import auth from "../middleware/auth.js"
+import { body } from "express-validator"
 
+const router = express.Router()
 
-
-
-const router = express.Router();
-
-router.post("/signup", [
+router.post(
+  "/signup",
+  [
     body("name").notEmpty().withMessage("First name is required").trim(),
     body("email", "Email is required").isEmail().normalizeEmail(),
     body("password", "Password is required and length min 4 chars.")
       .isLength({ min: 4 })
       .custom((val, { req }) => {
         if (val !== req.body.confirm_password) {
-          throw new Error("Password don't match!");
+          throw new Error("Password don't match!")
         } else {
-          return val;
+          return val
         }
       }),
-  ],createUser
-
+  ],
+  createUser
 )
 
-router.post("/login",loginUser)
+router.post("/login", loginUser)
 
 // router.get("/loggedin", authorize, loggedIn)
 
@@ -34,12 +39,11 @@ router.post("/history", postHistory)
 
 router.get("/history", getHistory)
 
-router.delete("/history/:id",deleteHistory)
+router.delete("/history/:id", deleteHistory)
 
-router.put("/history/:id",updateHistory)
+router.put("/history/:id", updateHistory)
 
-router.get("/autho", auth, loggedIn);
+router.get("/autho", auth, loggedIn)
 
 // router.get("/cache",getCache)
-export default router;
-
+export default router
